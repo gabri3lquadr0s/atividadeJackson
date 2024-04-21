f99c598088fd68174fae67bbb4dfb1ae3f144ff8
gabri3lquadr0s <gabrielquadrosrodapiao@gmail.com>
2024-04-21 09:54:46 -0300

    remaining att

--- js/index.js
+++ js/index.js
@@ -1,6 +1,4 @@
 $('h3').on('click', function() {
-    let area = $(this).text().toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, '');
-    let iframe = $('.iframe').attr('src').split('/')[2].split('.')[0].toLowerCase();
     let id = parseInt($(this).attr('id'));
     if(id == 0) {
         $('.iframe').attr('src', `./pages/home.html`);
